e84241dd43ca71b74b61f42f0dc3116b144856e8
Xojiakbar <ulugbekovhojiakbar157@email.com>
2026-05-29 22:18:59 +0500

    fix

---
+++ check.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', error => console.log('PAGE ERROR:', error.message));
+  
+  await page.goto('file://' + process.cwd() + '/www/index.html');
+  await new Promise(r => setTimeout(r, 2000));
+  await browser.close();
+})();
